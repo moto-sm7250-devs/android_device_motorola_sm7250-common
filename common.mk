6f60366a0814834f65db1d627fcefde06e2998a7
Vivekachooz <vivekachooz@gmail.com>
2023-12-20 00:50:49 +0530

    decomm powerhint
    Change-Id: Ibcc1fc45c0559adbf2b44c95fe4c2f81d7605fc1

--- common.mk
+++ common.mk
@@ -385,10 +385,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.sm7250-libperfmgr
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power-libperfmgr/powerhint_lagoon.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_lagoon.json \
-    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
