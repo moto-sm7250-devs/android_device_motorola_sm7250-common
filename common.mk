35c61a0961203a847164dc77bc66da398cc650a2
Vivekachooz <vivekachooz@gmail.com>
2023-12-19 13:35:23 +0530

    sm7250-common: power: Add powerhint for lagoon
    * Taken from kiev
    
    Change-Id: Ie5314de1ecab7e0233bb0cb1ba26bad06206394f

--- common.mk
+++ common.mk
@@ -386,6 +386,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.sm7250-libperfmgr
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/powerhint_lagoon.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_lagoon.json \
     $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QCOM
