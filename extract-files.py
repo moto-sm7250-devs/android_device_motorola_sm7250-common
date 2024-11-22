ad7a07d8805f91b27ef86b61a0e59da5e9337cf9
Vivekachooz <vivekachooz@gmail.com>
2024-11-22 09:02:51 +0530

    fixes
    Change-Id: Ica04bab5ac6cea3e56673a9a85063cda7586f055

--- extract-files.py
+++ extract-files.py
@@ -8,10 +8,44 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+namespace_imports = [
+    'device/motorola/sm7250-common',
+    'hardware/qcom-caf/sm8250',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libofflinelog',
+        'libqsap_sdk',
+        'libril',
+        'libthermalclient',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/permissions/moto-telephony.xml': blob_fixup()
@@ -30,6 +64,8 @@ module = ExtractUtilsModule(
     'sm7250-common',
     'motorola',
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
